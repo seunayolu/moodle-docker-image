565514c16ecbef0d57ddd1db7a17057adf7502b2
seunayolu <seunayolu@gmail.com>
2025-08-26 12:04:01 +0100

    moodle container setup with reverse proxy

---
+++ dockercompose.sh
@@ -0,0 +1,8 @@
+docker compose run --rm certbot certonly --webroot \
+  --webroot-path=/var/www/html \
+  -d runmoodle.teachdev.online \
+  --email ict@run.edu.ng \
+  --agree-tos \
+  --no-eff-email
+
+mkdir -p nginx/conf.d nginx/html letsencrypt
